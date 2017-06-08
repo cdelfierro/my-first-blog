85e7ae763a0c96f8226e7e009351c30d87b037ae
Carlos Gómez del Fierro <cdelfierro@gmail.com>
2017-06-08 00:31:00 -0400

    Adds security to the website: log in/out form & permissions to edit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4@-s**^@6tx7i83wpt*)ygw1kv&25$c5==h(ap15mxp7ivg(&!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com']
 
 
 # Application definition
@@ -120,3 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = '/'
